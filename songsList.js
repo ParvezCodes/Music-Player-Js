7d4d4a43d1d0a63d4869fe4db0f83e52d45bee5f
Parvezzzz <91866530+Parvezzzz@users.noreply.github.com>
2023-12-27 22:34:29 +0530

    songs added

--- songsList.js
+++ songsList.js
@@ -1,38 +1,44 @@
 const songsList = [
-    {
-      name: "Dandelions",
-      link: "./songs/dandelions.mp3",
-      artist: "Ruth B",
-      image: "./images/Dandelions.jpeg",
-    },
-    {
-      name: "Chaleya",
-      link: "./songs/chaleya.mp3",
-      artist: "Arijit Singh",
-      image: "./images/chaleya.jpg",
-    },
-    {
-      name: "Pathan",
-      link: "./songs/pathan.mp3",
-      artist: "Arijit Singh",
-      image: "./images/pathan.jpg",
-    },
-    {
-      name: "I Wanna Be Your",
-      link: "./songs/wannaBeYour.mp3",
-      artist: "Artic Monkeys",
-      image: "./images/wannaBeYour.jpeg",
-    },
-    {
-      name: "Perfect",
-      link: "./songs/perfect.mp3",
-      artist: "Ed Sheeran",
-      image: "./images/perfect.jpeg",
-    },
-    {
-      name: "Kalam Eineh",
-      link: "./songs/kalam.mp3",
-      artist: "Sherine",
-      image: "./images/kalam.png",
-    },
-  ];
+  {
+    name: "Dandelions",
+    link: "./songs/dandelions.mp3",
+    artist: "Ruth B",
+    image: "./images/Dandelions.jpeg",
+  },
+  {
+    name: "Pathan",
+    link: "./songs/pathan.mp3",
+    artist: "Arijit Singh",
+    image: "./images/pathan.jpg",
+  },
+  {
+    name: "I Wanna Be Your",
+    link: "./songs/wannaBeYour.mp3",
+    artist: "Artic Monkeys",
+    image: "./images/wannaBeYour.jpeg",
+  },
+  {
+    name: "Perfect",
+    link: "./songs/perfect.mp3",
+    artist: "Ed Sheeran",
+    image: "./images/perfect.jpeg",
+  },
+  {
+    name: "Kalam Eineh",
+    link: "./songs/kalam.mp3",
+    artist: "Sherine",
+    image: "./images/kalam.png",
+  },
+  {
+    name: "Dreamers",
+    link: "./songs/dreamers.mp3",
+    artist: "JungKook",
+    image: "./images/dreamers.jpeg",
+  },
+  {
+    name: "Greedy",
+    link: "./songs/greedy.mp3",
+    artist: "Tate McRae",
+    image: "./images/greedy.png",
+  },
+];
