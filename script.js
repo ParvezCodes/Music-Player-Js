b1c4d55e41ad2393a7c2122d4ee7454307ad034f
Parvezzzz <91866530+Parvezzzz@users.noreply.github.com>
2023-12-27 22:20:05 +0530

    updated

--- script.js
+++ script.js
@@ -23,38 +23,7 @@ let index;
 //initially loop=true
 let loop = true;
 
-const songsList = [
-  {
-    name: "Dandelions",
-    link: "./songs/dandelions.mp3",
-    artist: "Ruth B",
-    image: "./images/Dandelions.jpeg",
-  },
-  {
-    name: "Chaleya",
-    link: "./songs/chaleya.mp3",
-    artist: "Arijit Singh",
-    image: "./images/chaleya.jpg",
-  },
-  {
-    name: "Pathan",
-    link: "./songs/pathan.mp3",
-    artist: "Arijit Singh",
-    image: "./images/pathan.jpg",
-  },
-  {
-    name: "I Wanna Be Your",
-    link: "./songs/wannaBeYour.mp3",
-    artist: "Artic Monkeys",
-    image: "./images/wannaBeYour.jpeg",
-  },
-  {
-    name: "Perfect",
-    link: "./songs/perfect.mp3",
-    artist: "Ed Sheeran",
-    image: "./images/perfect.jpeg",
-  },
-];
+
 
 //events object
 let events = {
---
+++ songsList.js
@@ -0,0 +1,38 @@
+const songsList = [
+    {
+      name: "Dandelions",
+      link: "./songs/dandelions.mp3",
+      artist: "Ruth B",
+      image: "./images/Dandelions.jpeg",
+    },
+    {
+      name: "Chaleya",
+      link: "./songs/chaleya.mp3",
+      artist: "Arijit Singh",
+      image: "./images/chaleya.jpg",
+    },
+    {
+      name: "Pathan",
+      link: "./songs/pathan.mp3",
+      artist: "Arijit Singh",
+      image: "./images/pathan.jpg",
+    },
+    {
+      name: "I Wanna Be Your",
+      link: "./songs/wannaBeYour.mp3",
+      artist: "Artic Monkeys",
+      image: "./images/wannaBeYour.jpeg",
+    },
+    {
+      name: "Perfect",
+      link: "./songs/perfect.mp3",
+      artist: "Ed Sheeran",
+      image: "./images/perfect.jpeg",
+    },
+    {
+      name: "Kalam Eineh",
+      link: "./songs/kalam.mp3",
+      artist: "Sherine",
+      image: "./images/kalam.png",
+    },
+  ];
